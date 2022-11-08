4b24485ed4eef0acee809a90509e7c42a050da1c
elB1MBO <raulduran.lacimada@gmail.com>
2022-11-08 18:09:05 +0100

    Codigo base para usar datos

--- src/pages/DishTypes.tsx
+++ src/pages/DishTypes.tsx
@@ -1,12 +1,66 @@
-import { IonContent, IonSegment, IonSegmentButton, IonTabBar, IonLabel, IonIcon, IonTabButton, IonItem, IonButton, IonImg } from '@ionic/react';
+import { IonContent, IonList, IonGrid, IonSegment, IonSegmentButton, IonTabBar, IonLabel, IonIcon, IonTabButton, IonItem, IonButton, IonImg, IonRow } from '@ionic/react';
 import Header from '../components/Header';
 import './DishTypes.css';
-import { useState } from "react";
-import { star, addCircleOutline, checkmarkOutline } from 'ionicons/icons';
+import { useState, useEffect } from "react";
+import { star, addCircleOutline, checkmarkOutline, body, menu } from 'ionicons/icons';
+import axios from 'axios';
+import { API_URL } from '../variables';
+{/** Para obtener datos de la API: 
+  *  import { API_URL } from '../variables';
+*/}
+
+{/** Que es axios? --> Axios is an HTTP client library that allows you 
+to make requests to a given endpoint */}
+
+{/** DATA: no puedes poner imports debajo de uno de estos comentarios ya que 
+lo reconoce como parte del body del código, no de la parte de los imports */}
+
 const DishTypes: React.FC = () => {
 
+  {/** Comprueba si está en menú (si no, entonces está en postres) */}
   const [menusActive, setMenusActive] = useState<boolean>(true);
-  const [desertsActive, setDesertsActive] = useState<boolean>(false);
+  {/** Para los datos de menus y postres */}
+  const [menus, setMenus] = useState([]);
+  const [deserts, setDeserts] = useState([]);
+  const [postMenu, setPostMenu] = useState(null);
+  const [postDesert, setPostDesert] = useState(null); 
+
+  {/** Queremos que obtenga los menús y postres de la base de datos, y que cree nuevos 
+con el boton de Añadir */}
+  const sendGetRequest = () => {
+    return axios({
+      url: API_URL + "dishtypes",
+      method: 'get'
+    }).then(response => {
+      console.log(response.data);
+      return(response.data);
+    })
+  };
+  {/** useEffect Hook para usar el get con Axios y obtener los datos de la url asignada antes*/}
+  useEffect(() => {
+    sendGetRequest().then(data => {
+      {/** Aqui habria que obtener los datos de postres y menús, que no sé si 
+        están en 2 tablas distintas o estan en la misma y hay que separar los datos */}
+      setMenus(data)
+      /* setDeserts(data) */
+    })
+  }, [])
+
+  {/** Para hacer POST de un nuevo menu o postre */}
+  useEffect(() => {
+    axios.get(API_URL).then((respone) => {
+      setPostMenu(respone.data);
+    })
+  }, []); {/** El [] es para indicar los valores que se aplica el efecto*/}
+
+  const createPost = () => {
+    axios.post(API_URL, {
+      //Cuerpo del post, datos del menu (nombre)
+      name: "Nuevo menú",
+    }).then((response) => {
+      setPostMenu(response.data);
+    })
+  };
 
   return (
     <>
@@ -14,13 +68,11 @@ const DishTypes: React.FC = () => {
       <IonContent fullscreen>
         <IonSegment>
           <IonSegmentButton value="menus" onClick={() => {
-                  setDesertsActive(false);
                   setMenusActive(true);
                 }}>
             <IonLabel class="btn-title">Menús</IonLabel>
           </IonSegmentButton>
           <IonSegmentButton value="postres" onClick={() => {
-                  setDesertsActive(true);
                   setMenusActive(false);
                 }}>
             <IonLabel class="btn-title">Postres</IonLabel>
@@ -33,17 +85,42 @@ const DishTypes: React.FC = () => {
            */}
           {menusActive ? (
             <>
-              <IonButton expand="full" class="tab-list">
-              <IonImg slot='start' class="pictogram-header" src="https://api.arasaac.org/api/pictograms/2398?resolution=500&download=false" ></IonImg>
-                Menu 1
-              </IonButton>
-              
-              <IonButton expand="full" class="tab-list">
-              <IonIcon slot="start" icon={star}></IonIcon>
-                Menu 2
-              </IonButton>
+              {/* <IonGrid>
+                {
+                  menus.map(menu => {
+                    return (
+                      <IonRow class='ion-justify-content-left'>
+                        Podemos crear una clase/tipo de boton para esto mejor que el por defecto
+                        <IonButton expand="full" class="tab-list" href='#'>
+                          <IonImg slot='start' class="pictogram-header" src={menu['_accessible_element']['_image']}></IonImg>
+                          {menu['_accesible_element']['_text']}
+                        </IonButton>
+                      </IonRow>
+                    )
+                  })
+                }
+              </IonGrid> 
+              */}
 
-              <IonButton class="add-button" color="blue" fill="outline" shape="round" expand="block">
+              <IonList>
+                <IonItem>
+                  <IonButton expand="full" class="tab-list">
+                  <IonImg slot='start' class="pictogram-header" src="https://api.arasaac.org/api/pictograms/2398?resolution=500&download=false" ></IonImg>
+                  Menu 1
+                  </IonButton>
+                </IonItem>
+                <IonItem>
+                  <IonButton expand="full" class="tab-list">
+                    <IonIcon slot="start" icon={star}></IonIcon>
+                    Menu 2
+                  </IonButton>
+                </IonItem>
+              </IonList>
+              
+              {/**
+               * <IonButton class="add-button" color="blue" fill="outline" shape="round" expand="block" onClick={createPost}>
+              */}
+              <IonButton class="add-button" color="blue" fill="outline" shape="round" expand="block" >
               <IonIcon slot="start" icon={addCircleOutline}></IonIcon>
                 Añadir Menú
               <IonIcon slot="end" icon={checkmarkOutline}></IonIcon>
@@ -51,16 +128,26 @@ const DishTypes: React.FC = () => {
             </>
           ) : (
             <>
-              <IonButton expand="full" class="tab-list">
-              <IonIcon slot="start" icon={star}></IonIcon>
-                Postre 1
-              </IonButton>
+              <IonList>
+                <IonItem>
+                  <IonButton expand="full" class="tab-list">
+                  <IonImg slot='start' class="pictogram-header" src="https://api.arasaac.org/api/pictograms/2398?resolution=500&download=false" ></IonImg>
+                  Postre 1
+                  </IonButton>
+                </IonItem>
+                <IonItem>
+                  <IonButton expand="full" class="tab-list">
+                    <IonIcon slot="start" icon={star}></IonIcon>
+                    Postre 2
+                  </IonButton>
+                </IonItem>
+              </IonList>
               
-              <IonButton expand="full" class="tab-list">
-              <IonIcon slot="start" icon={star}></IonIcon>
-                Postre 2
+              <IonButton class="add-button" color="blue" fill="outline" shape="round" expand="block">
+              <IonIcon slot="start" icon={addCircleOutline}></IonIcon>
+                Añadir Postre
+              <IonIcon slot="end" icon={checkmarkOutline}></IonIcon>
               </IonButton>
-              <IonButton class="add-button">Añadir Postre</IonButton>
             </>
           )}
         </IonContent>
