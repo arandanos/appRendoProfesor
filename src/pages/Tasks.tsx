9e8064e595153b54274cc9fbbf18c761866ad5f0
Antonio Quirante <antonioqh.github@gmail.com>
2022-11-09 20:17:29 +0100

    Pagina Lista Tareas con API Funcionando

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,12 +1,49 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonGrid, IonItem, IonLabel, IonIcon, IonRow } from '@ionic/react';
 import './Tasks.css';
 import Header from '../components/Header';
+import { API_URL } from '../variables';
+import { settingsOutline } from 'ionicons/icons';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal} from 'react';
+import TaskList from '../components/TasksList';
 
 const Tasks: React.FC = () => {
+
+  const [tasks, setTasks] = useState([]);
+ 
+
+  const sendGetRequest = () => {
+
+    return axios({
+      url: API_URL + "task",
+      method: 'get'
+    }).then(response => {       
+     // console.log(response.data)
+      return response.data;
+    })
+  };
+
+
+  useEffect(() =>{
+    sendGetRequest().then(data => {
+      setTasks(data)      
+    })    
+  }, []) 
+  
+  console.log(tasks[1])
+
   return (
     <IonPage>
       <Header title="Tareas" settings back={false}/>
       <IonContent fullscreen>
+      <IonGrid>
+        {tasks.map(element => {
+              return (
+                  <TaskList text={element['_accessible_element']['_text']} pictogram={element['_accessible_element']['_pictogram']}/>
+              );
+          })}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
