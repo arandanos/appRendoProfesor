c0a14cd2b874e03c97fc82019dfee85c89f21b2e
AntonioQuirante <antonioqh.github@gmail.com>
2022-11-18 12:49:48 +0100

    new

--- src/pages/MaterialTask.tsx
+++ src/pages/MaterialTask.tsx
@@ -1,5 +1,8 @@
-import { IonContent } from '@ionic/react';
+import { IonFab, IonIcon, IonFabButton, IonContent, IonList, IonItem, IonLabel, IonSelect, IonSelectOption, IonGrid, IonRow, IonCol } from '@ionic/react';
 import Header from '../components/Header';
+import { checkmark, arrowBackOutline, arrowForwardOutline, add } from 'ionicons/icons';
+import ToggleSwitch from '../components/ToggleSwitch';
+import CalendarPicker from '../components/CalendarPicker';
 import './Pages.css'
 
 const MaterialTask: React.FC = () => {
@@ -7,6 +10,43 @@ const MaterialTask: React.FC = () => {
     <>
       <Header title="Material" back settings={false}/>
       <IonContent fullscreen>
+      <IonList>
+          <div className="width-90">
+            <CalendarPicker label='Seleccionar fecha límite' disabled={false} editButton={false} value=''/>
+
+            <IonLabel>Seleccionar alumno</IonLabel>
+            <IonItem shape="round" fill="outline">
+              <IonSelect interface="popover" placeholder="Alumno">
+                <IonSelectOption value="apples">Manuel García</IonSelectOption>
+                <IonSelectOption value="oranges">Franciso Barrios</IonSelectOption>
+                <IonSelectOption value="bananas">Antonio Suárez</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+
+            <IonGrid>
+              <IonRow>
+                
+                <IonGrid>
+                  <IonRow>
+                      <IonCol>Cantidad</IonCol>
+                      <IonCol>Material</IonCol>
+                  </IonRow>
+                  <IonRow>
+                      Tipo de material                
+                  </IonRow>
+                </IonGrid>
+              </IonRow>
+            </IonGrid>
+            
+            <IonFabButton color="success" size='small'>
+                      <IonIcon icon={add}></IonIcon>
+            </IonFabButton>            
+
+            <ToggleSwitch label='Feedback automático' checked={false}/>
+            <ToggleSwitch label='Comentarios' checked/>
+
+          </div>
+        </IonList>
       </IonContent>
     </>
   );
