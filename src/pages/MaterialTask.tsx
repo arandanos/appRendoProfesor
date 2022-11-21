ad2adedb83468adad97c195bed8fd0a655597ae6
AntonioQuirante <antonioqh.github@gmail.com>
2022-11-21 10:39:44 +0100

    Problema con los hooks

--- src/pages/MaterialTask.tsx
+++ src/pages/MaterialTask.tsx
@@ -1,18 +1,20 @@
-import { IonFab, IonIcon, IonFabButton, IonContent, IonList, IonItem, IonLabel, IonSelect, IonSelectOption, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonPage,IonFab, IonIcon, IonFabButton, IonContent, IonList, IonItem, IonLabel, IonSelect, IonSelectOption, IonGrid, IonRow, IonCol } from '@ionic/react';
 import Header from '../components/Header';
-import { checkmark, arrowBackOutline, arrowForwardOutline, add } from 'ionicons/icons';
+import { checkmark, arrowBackOutline, arrowForwardOutline, add, trash } from 'ionicons/icons';
 import ToggleSwitch from '../components/ToggleSwitch';
 import CalendarPicker from '../components/CalendarPicker';
 import './Pages.css'
+import { useState } from 'react';
 
 const MaterialTask: React.FC = () => {
+
   return (
-    <>
+    <IonPage>
       <Header title="Material" back settings={false}/>
-      <IonContent fullscreen>
+      <IonContent fullscreen class="scroll-content">      
       <IonList>
           <div className="width-90">
-            <CalendarPicker label='Seleccionar fecha límite' disabled={false} editButton={false} value=''/>
+            <CalendarPicker label='Selecciona fecha de la tarea' disabled={false} editButton={false} value=''/>
 
             <IonLabel>Seleccionar alumno</IonLabel>
             <IonItem shape="round" fill="outline">
@@ -24,23 +26,64 @@ const MaterialTask: React.FC = () => {
             </IonItem>
 
             <IonGrid>
+
               <IonRow>
-                
                 <IonGrid>
                   <IonRow>
-                      <IonCol>Cantidad</IonCol>
-                      <IonCol>Material</IonCol>
+                      <IonCol>
+                      <IonLabel>Cantidad</IonLabel>
+                        <IonItem shape="round" fill="outline">
+                          <IonFabButton size='small' >
+                              <IonIcon icon={add}></IonIcon>
+                          </IonFabButton>
+                          0
+                        </IonItem>
+                      </IonCol>
+
+
+                      
+
+                      <IonCol>
+                      <IonLabel>Material</IonLabel>
+                      <IonItem shape="round" fill="outline">
+                      <IonSelect interface="popover" placeholder="0">
+                            <IonSelectOption>1</IonSelectOption>
+                            <IonSelectOption>2</IonSelectOption>
+                            <IonSelectOption>3</IonSelectOption>
+                          </IonSelect>
+                      </IonItem>
+                      </IonCol>
                   </IonRow>
+
                   <IonRow>
-                      Tipo de material                
+                      <IonLabel>Color</IonLabel>
+                      <IonItem shape="round" fill="outline">
+                        <IonSelect interface="popover" placeholder="Selecciona un tipo">
+                            <IonSelectOption>Rojo</IonSelectOption>
+                            <IonSelectOption>Amarillo</IonSelectOption>
+                            <IonSelectOption>Verde</IonSelectOption>
+                          </IonSelect>
+                      </IonItem>
+
+                      <IonFabButton color="danger" size='small'>
+                        <IonIcon icon={trash}></IonIcon>
+                      </IonFabButton>           
                   </IonRow>
                 </IonGrid>
               </IonRow>
+
+              <IonRow text-center>
+                <IonCol className='ButtonSuccess'>
+                  <IonFabButton color="success" size='small'>
+                          <IonIcon icon={add}></IonIcon>
+                  </IonFabButton>
+                </IonCol>                
+              </IonRow>
+
             </IonGrid>
+
             
-            <IonFabButton color="success" size='small'>
-                      <IonIcon icon={add}></IonIcon>
-            </IonFabButton>            
+                        
 
             <ToggleSwitch label='Feedback automático' checked={false}/>
             <ToggleSwitch label='Comentarios' checked/>
@@ -48,7 +91,7 @@ const MaterialTask: React.FC = () => {
           </div>
         </IonList>
       </IonContent>
-    </>
+    </IonPage>
   );
 };
 
