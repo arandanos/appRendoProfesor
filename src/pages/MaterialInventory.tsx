a3c64c968f3efa957dd04ae882a8d1222f7319a5
cmarinre <cmarinre@correo.ugr.es>
2022-11-24 22:25:51 +0100

    add MaterialInventory but without links and without ARAND-368 MaterialTaskView

---
+++ src/pages/MaterialInventory.tsx
@@ -0,0 +1,49 @@
+import { IonContent, IonGrid, IonPage, IonSearchbar } from '@ionic/react';
+import './Pages.css';
+import Header from '../components/Header';
+import './KitchenOrderView.css'
+
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { API_URL } from '../variables';
+import ListItem from '../components/ListItem';
+const baseURL = "http://localhost:8000/api/task/2";
+
+const MaterialInventory: React.FC = () => {
+    
+    const [materials, setMaterials] = useState([]);
+    
+    const sendGetRequest =  () => {
+        return axios({
+            url: API_URL + "material",
+            method: 'get'
+        }).then(response => {
+            return response.data;
+        })
+    };
+
+    useEffect(()=>{
+        sendGetRequest().then(data => {
+            setMaterials(data)
+        })
+    },[])
+    
+
+	return (
+		<IonPage>
+			<Header title="Almacén" settings={false} back={true} />
+			<IonContent fullscreen>
+            <IonGrid class='list-container'>
+                <IonSearchbar showClearButton="focus" placeholder="Buscar material..."></IonSearchbar>
+                {materials.map((element : any) => {
+                        return (
+                            <ListItem text={element['_accessible_element']['_text']} pictogram={element['_accessible_element']['_pictogram']}/>
+                        );
+                    })}
+                </IonGrid>
+			</IonContent>
+		</IonPage>
+	);
+};
+
+export default MaterialInventory;
