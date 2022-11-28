c464406f490c785860e20a16ecbc16b0e019c9d0
cmarinre <cmarinre@correo.ugr.es>
2022-11-28 17:56:28 +0100

    MaterialInventoryDisponibility with API

--- src/pages/MaterialInventoryDisponibility.tsx
+++ src/pages/MaterialInventoryDisponibility.tsx
@@ -9,6 +9,9 @@ import axios from "axios";
 import { API_URL } from '../variables';
 import ListItem from '../components/ListItem';
 import { useParams } from 'react-router';
+import DisponibilityList from '../components/DisponibilityList';
+import MaterialList from '../components/MaterialList';
+import { url } from 'inspector';
 const baseURL = "http://localhost:8000/api/task/2";
 
 const MaterialInventoryDisponibility: React.FC = () => {
@@ -18,21 +21,65 @@ const MaterialInventoryDisponibility: React.FC = () => {
         id_material: string;
     }
     const {id_material} = useParams<params>();
+
+
+    /*para los datos de materiales*/
+    const [materials, setMaterials] = useState([]);
+    const [showLoading, setShowLoading] = useState(true);
     
-    //const id_material  = useParams();
+    /*Queremos que obtenga los materiales de la base de datos) */
+    const sendGetRequest =  () => {
+        return axios({
+            url: API_URL + "material",
+            method: 'get'
+        }).then(response => {
+            return response.data;
+        })
+    };
+
+    useEffect(()=>{
+        sendGetRequest().then(data => {
+            setMaterials(data)
+            setIsLoading(false)
+            setShowLoading(false)
+        })
+    },[])
+    
+    var arrayElementos: Array<JSX.Element> = [];
+    const [isLoading, setIsLoading] = useState(true)
+    var nombreMaterial = "aA"
+    function findMaterials(materials: any[]) {
+        let materialesFiltrados = materials.filter(  material => material['_type']['_id'] == id_material )
+       /* var copia = materials.flat(0)
+        nombreMaterial = copia['_type']['_item']['_text'] */
+        return materialesFiltrados
+    }
+
+    var materialesF = findMaterials(materials) 
+
+    arrayElementos = [
+        <>
+            <IonGrid class="list-container-materials">
+                {
+                    materialesF.map(material => {
+                        return (
+                            <DisponibilityList text={material['_color']['_text']} pictogram={material['_color']['_pictogram']} quantity={material['_quantity']}></DisponibilityList>
+                        )
+                        })
+                }
+            </IonGrid>
+        </>
+    ]
+
+
+    
     return (
             <IonNav root={() =>
                 <IonPage>
-                    <Header title= {"Almacén: " + id_material}  back settings={false}  />
+                    <Header title= {"Almacén: " + nombreMaterial}  back settings={false}  />
                     <IonContent fullscreen>
                     <IonGrid class='list-container'>
-                        <IonSearchbar showClearButton="focus" placeholder="Buscar material..."></IonSearchbar>
-
-                    {/*  {materials.map((element : any) => {
-                                return (
-                                    <ListItem text={element['_accessible_element']['_text']} pictogram={element['_accessible_element']['_pictogram']}/>
-                                );
-                            })}*/}
+                        <MaterialList tabsComponents={arrayElementos}></MaterialList>
                         </IonGrid>
                     </IonContent>
                 </IonPage>
