44d17714b9c6e77a4735ebcf1277951be5b80b44
cmarinre <cmarinre@correo.ugr.es>
2022-11-30 12:49:56 +0100

    almost searchBar

--- src/pages/MaterialInventory.tsx
+++ src/pages/MaterialInventory.tsx
@@ -1,13 +1,11 @@
-import { IonContent, IonGrid, IonLabel, IonSearchbar } from '@ionic/react';
+import { IonContent, IonGrid, IonItem, IonLabel, IonList, IonSearchbar } from '@ionic/react';
 import './Pages.css';
 import Header from '../components/Header';
 import './MaterialInventory.css'
 import '../ApiMethods'
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
 import ListItem from '../components/ListItem';
 import { sendGetAllRequest } from '../ApiMethods';
-const API_URL = ("http://localhost:8000/api/");
 
 
 const MaterialInventory: React.FC = () => {
@@ -15,16 +13,15 @@ const MaterialInventory: React.FC = () => {
     /*para los datos de materiales*/
     const [materials, setMaterials] = useState([]);
     
-    /*Queremos que obtenga los materiales de la base de datos) */
-    
-
     useEffect(()=>{
         sendGetAllRequest("material").then(data => {
             setMaterials(data)
         })
     },[])
     
-    var arrayElementos: Array<JSX.Element> = [];
+    var arrayAuxiliarNombres: Array<string> = [];
+    let [prueba, setPrueba] = useState(Array<any>)
+
 
     function findMaterials(materials: any[]) {
         var tipos: any[] = []
@@ -34,37 +31,58 @@ const MaterialInventory: React.FC = () => {
             if (tipos.find(element => element == tipo) == undefined){
                 tipos.push(tipo)
                 materialesFiltrados.push(material)
+                arrayAuxiliarNombres.push(String(material['_type']['_item']['_text']))
             }
-        }   
+        }
         )
+ /*       prueba = materialesFiltrados*/
         return materialesFiltrados
     }
+
     var materialesF = findMaterials(materials) 
+    
+    
+    function searchBarMaterials(materials: any[], arrayAuxiliarNombres: any[]) {
+        console.log(materials)
+        var arrayElementosBarraBuscadora: Array<JSX.Element> = [];
+        materials.map( material =>{
+            if(arrayAuxiliarNombres.find(nombre => nombre == material['_type']['_item']['_text'])!=undefined){
+                arrayElementosBarraBuscadora.push(material)
+            }    
+        }
+        )
+        console.log(arrayElementosBarraBuscadora)
+        return arrayElementosBarraBuscadora
+    }    
 
 
-    arrayElementos = [
-        <>
-            <IonGrid class="list-container-materials">
-                {
-                    materialesF.map(material => {
-                        return (
-                            <ListItem text={material['_type']['_item']['_text']} pictogram={material['_type']['_item']['_pictogram']} href={"/MaterialInventoryDisponibility/"+ material['_type']['_id']}></ListItem>
-                        )
-                        })
-                }
-            </IonGrid>
-        </>
-    ]
+    const handleChange = (ev: Event) => {
+        let query = "";
+        const target = ev.target as HTMLIonSearchbarElement;
+        if (target) query = target.value!.toLowerCase();
+        arrayAuxiliarNombres = arrayAuxiliarNombres.filter(d => d.toLowerCase().indexOf(query) > -1)
+        setPrueba(searchBarMaterials(materialesF, arrayAuxiliarNombres))
+    }
+  
 
     return (
         <>
             <Header title="Almacén" back settings={false}  />
-            <IonSearchbar showClearButton="focus" placeholder="Buscar material...">
-            </IonSearchbar> 
 
             <IonContent fullscreen>
-                 <IonLabel>{arrayElementos}</IonLabel>
-             </IonContent>
+                <IonSearchbar showClearButton="focus" placeholder="Buscar material..." debounce={1000} onIonChange={(ev) => handleChange(ev)}></IonSearchbar>
+                
+                <IonGrid class="list-container-materials">
+                    {
+                        prueba.map(material => {
+                            return (
+                                <ListItem text={material['_type']['_item']['_text']} pictogram={material['_type']['_item']['_pictogram']} href={"/MaterialInventoryDisponibility/"+ material['_type']['_id']}></ListItem>
+                            )
+                            })
+                    }
+                </IonGrid>
+
+            </IonContent>
         </>
     );
 
