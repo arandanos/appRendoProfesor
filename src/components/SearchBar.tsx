2772d1a17258d5a699bb3013995edef4129a9bbb
Marina Muñoz <marinamuca01@gmail.com>
2022-12-01 20:43:24 +0100

    Add Disclaimer on SearchBar Component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -13,6 +13,9 @@ const SearchBar: React.FC<SearchBarProps> = ( props : SearchBarProps) => {
     const resultsFilter = (element: any[], query : string) => {
         var results : any = [];
         element.map( element => {
+            // ! Importante: solo funciona para los elementos de la base de datos cuya clave 
+            // ! externa de accessible element se llame name, si en algun momento se necesita
+            // ! para otro caso comunicarlo a quien esté haciendo la API para buscar una solución
             if (element['_name']['_text'].toLowerCase().includes(query.toLowerCase())){
                 results.push(element);
             }
