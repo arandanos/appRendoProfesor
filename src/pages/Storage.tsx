359a649e4f33a9898cb60ab672393741139cdc9d
Marina Muñoz <marinamuca01@gmail.com>
2022-12-06 21:11:52 +0100

    Use SearchBar Component on Storage

--- src/pages/Storage.tsx
+++ src/pages/Storage.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonGrid, IonItem, IonLabel, IonList, IonSearchbar, SearchbarChangeEventDetail } from '@ionic/react';
+import { IonContent, IonGrid, IonPage, IonSearchbar } from '@ionic/react';
 import './Pages.css';
 import Header from '../components/Header';
 import './MaterialInventory.css'
@@ -6,85 +6,57 @@ import '../ApiMethods'
 import React, { useEffect, useState } from 'react';
 import ListItem from '../components/ListItem';
 import { sendGetAllRequest } from '../ApiMethods';
+import SearchBar from '../components/SearchBar';
 
 
 const Storage: React.FC = () => {
     
     /*para los datos de materiales*/
     const [materials, setMaterials] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ results, setResults ] = useState<any>([]);
   
     
     useEffect(()=>{
-
-        sendGetAllRequest("material").then(data => {
-            setMaterials(data)
+        sendGetAllRequest("material_type").then(data => {
+            setMaterials(data);
+            setResults(data); 
+            setIsLoading(false);  
         })
     },[])
 
-
-    let [prueba, setPrueba] = useState(Array<any>)
-    var arrayAuxiliarNombres: Array<string> = [];
-
-
-    function findMaterials(materials: any[]) {
-        var tipos: any[] = []
-        var materialesFiltrados : any[] = []
-        materials.map( material =>{
-            var tipo = material['_type']['_id']
-            if (tipos.find(element => element == tipo) == undefined){
-                tipos.push(tipo)
-                materialesFiltrados.push(material)
-                arrayAuxiliarNombres.push(String(material['_type']['_item']['_text']))
-            }
-        }
-        )
-        return materialesFiltrados
+    const updateResults = (results:any)=>{
+        setResults(results);
     }
 
-    var materialesF = findMaterials(materials) 
-    
-    
-    function searchBarMaterials(materials: any[], arrayAuxiliarNombres: any[]) {
-        var arrayElementosBarraBuscadora: Array<JSX.Element> = [];
-        materials.map( material =>{
-            if(arrayAuxiliarNombres.find(nombre => nombre == material['_type']['_item']['_text'])!=undefined){
-                arrayElementosBarraBuscadora.push(material)
-            }    
-        }
-        )
-        console.log(arrayElementosBarraBuscadora)
-        return arrayElementosBarraBuscadora
-    }    
-
-
-    const handleChange = (ev: Event) => {
-        let query = "";
-        const target = ev.target as HTMLIonSearchbarElement;
-        if (target) query = target.value!.toLowerCase();
-        arrayAuxiliarNombres = arrayAuxiliarNombres.filter(d => d.toLowerCase().indexOf(query) > -1)
-        setPrueba(searchBarMaterials(materialesF, arrayAuxiliarNombres))
-    }
-  
+    if(isLoading) {
+        // * AQUI IRA EL SPLASH DE CARGA
+        return(
+          <IonPage>
+            <h1>Cargando...</h1>
+          </IonPage>
+        );
+    } 
 
     return (
-        <>
+        <IonPage>
             <Header title="Almacén" back settings={false}  />
 
             <IonContent fullscreen>
-                <IonSearchbar showClearButton="focus" placeholder="Buscar material..." debounce={1000} onIonChange={(ev) => handleChange(ev)}></IonSearchbar>
-                
+                {/* <IonSearchbar showClearButton="focus" placeholder="Buscar material..." debounce={1000} onIonChange={(ev) => handleChange(ev)}></IonSearchbar> */}
+                <SearchBar elements={materials} updateResults={updateResults}></SearchBar>
                 <IonGrid class="list-container-materials">
                     {
-                        prueba.map(material => {
+                        results.map((material:any) => {
                             return (
-                                <ListItem text={material['_type']['_item']['_text']} pictogram={material['_type']['_item']['_pictogram']} href={"/storage/"+ material['_type']['_id']}></ListItem>
+                                <ListItem text={material['_name']['_text']} pictogram={material['_name']['_pictogram']} href={"/storage/"+ material['_id']}></ListItem>
                             )
                             })
                     }
                 </IonGrid>
 
             </IonContent>
-        </>
+        </IonPage>
     );
 
 };
