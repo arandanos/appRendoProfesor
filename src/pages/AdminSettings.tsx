f635c5b58146ddcb9473b6f1342435bd0936b66f
Marina Muñoz <marinamuca01@gmail.com>
2022-12-15 23:16:34 +0100

    Add Admin Settings

--- src/pages/AdminSettings.tsx
+++ src/pages/AdminSettings.tsx
@@ -11,6 +11,8 @@ const AdminSettings: React.FC = () => {
           <IonGrid class='button-grid'>
             <StyledButton label="Tipos de menús y postres" href="/dish_types"/>
             <StyledButton label="Gestión de Usuarios" href="#"/>
+            <StyledButton label="Gestión de Pictogramas" href="#"/>
+            <StyledButton label="Gestión de Colores" href="#"/>
             <StyledButton label="Gestión de clases" href="/classrooms"/>
             <StyledButton label="Materiales del almacén" href="/storage"/>
           </IonGrid>
